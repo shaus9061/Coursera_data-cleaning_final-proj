0dcf699ad81415df25c09293945643a03a27d483
shaus9061 <shaus@vt.edu>
2017-08-09 15:31:03 -0400

    run_analysis script

---
+++ run_analysis.R
@@ -0,0 +1,40 @@
+library(dplyr)
+
+filename = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(filename, "tempfile.zip")
+paths <- unzip("tempfile.zip")
+labels <- read.table(paths[2])
+names(labels) <- c("num", "variable")
+variables <- labels$variable
+variables <- gsub("\\()","",as.character(variables))
+variables <- gsub("\\,"," ",as.character(variables))
+
+test <- data.frame(read.table(paths[15]))
+names(test) <- variables 
+test$subject <- readLines(paths[14])
+test$activity <- readLines(paths[16])
+
+train <- data.frame(read.table(paths[27]))
+names(train) <- variables
+train$subject <- readLines(paths[26])
+train$activity <- readLines(paths[28])
+
+all <-rbind(test, train)
+activity <- all$activity
+subject <- all$subject
+
+activity <- gsub("1", "Walking", activity)
+activity <- gsub("2", "Walking_Upstairs", activity)
+activity <- gsub("3", "Walking_Downstairs", activity)
+activity <- gsub("4", "Sitting", activity)
+activity <- gsub("5", "Standing", activity)
+activity <- gsub("6", "Laying", activity)
+
+
+allmsd <- cbind(all[, grep("mean", names(all))], all[, grep("std", names(all))])
+allmsd <-cbind(subject, allmsd)
+allmsd <- cbind(activity, allmsd)
+
+activitymeantable <- allmsd %>% group_by(activity) %>% summarise_all(mean)
+subjectmeantable <- allmsd %>% group_by(subject) %>% summarise_all(mean)
+meantable <- allmsd %>% group_by(subject, activity) %>% summarise_all(mean)
